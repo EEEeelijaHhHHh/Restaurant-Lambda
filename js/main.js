936097bbc91bc04e0f9eb76dc232c0db73a4932d
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-09-13 12:05:42 +0300

    Added animated menu hamburger

--- js/main.js
+++ js/main.js
@@ -12,3 +12,21 @@
     });
   });
 }());
+
+/* Hamburger Menu */
+(function () {
+  const button = document.querySelector('.nav__btn');
+  const menu = document.querySelector('.nav-menu');
+  const menuLink = document.querySelectorAll('.nav-menu__link');
+
+  button.addEventListener('click', () => toggleMenu());
+
+  menuLink.forEach(link => {
+    link.addEventListener('click', () => toggleMenu());
+  });
+
+  function toggleMenu() {
+    menu.classList.toggle('nav-menu_active');
+    button.classList.toggle('nav__btn_active');
+  };
+}());
